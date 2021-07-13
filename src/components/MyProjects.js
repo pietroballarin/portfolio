604d537d791f3549b773336b485e16116e541af6
Pietro Ballarin <pietroballarin@outlook.com>
2021-07-13 17:07:21 +0200

    creating projects section

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -3,17 +3,20 @@ import { projects } from '../data';
 
 export default function MyProjects() {
     return (
-        <section id="projects">
-            {projects.map(project =>
-            <div className="project-box">
-                <div className="box-details">     
-                    <h2>{project.title}</h2>
-                    <img src={project.image} alt={project.altimg}/>
-                    <h3>{project.stack}</h3>
-                    <p>{project.description}</p>
+        <section id="my-projects">
+            <h1>MY PROJECTS</h1>
+            <div className="projects">
+                {projects.map(project =>
+                <div className="project-box">
+                    <div className="box-details">     
+                        <h2>{project.title}</h2>
+                        <img src={project.image} alt={project.altimg}/>
+                        <h3>{project.stack}</h3>
+                        <p>{project.description}</p>
+                    </div>
                 </div>
+                )}
             </div>
-            )}
         </section>
     )
 }
